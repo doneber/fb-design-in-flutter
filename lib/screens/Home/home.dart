7189328357f966549f0b8416ebe4bd60ffd16aab
Eber <eber.quenta@jala-foundation.org>
2021-04-25 23:07:24 -0400

    Add basic structure

---
+++ lib/screens/Home/home.dart
@@ -0,0 +1,13 @@
+import 'package:fb_design/screens/Home/components/header.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Header()],
+      ),
+    );
+  }
+}
